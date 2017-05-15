482f6720a638d12c0436ae75796752e256d090f2
jesniphat <jesniphat@hotmail.com>
2017-05-15 07:49:22 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gitignore
    #	README.md
    #	e2e/app.e2e-spec.ts
    #	e2e/app.po.ts
    #	karma.conf.js
    #	package.json
    #	protractor.conf.js
    #	src/app/app.component.spec.ts
    #	src/app/app.module.ts
    #	src/environments/environment.ts
    #	src/index.html
    #	src/main.ts
    #	src/polyfills.ts
    #	src/test.ts
    #	src/typings.d.ts
    #	tslint.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(
+    public _elRef: ElementRef
+  ) {
+
+  }
+
+  ngOnInit() {
+    console.log("start");
+  }
 }
