8314498aadc4cc1ebb0d4e55684f90f64687193f
jesniphat <jesniphat@hotmail.com>
2017-06-08 08:46:40 +0700

    edit home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from "../../service/api.service";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public slideHead: any[];
+  public imgLink: string = "";
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    console.log("start home");
+
+    this.imgLink = this.apiService.img;
+    this.getThreeProduct();
+  }
+
+  public getThreeProduct() {
+    let param = {};
+    this.apiService
+      .post("/api/getTheerProduct", param)
+      .subscribe(
+      data => this.getThreeProductDoneAction(data), // OR this.categoryLists = data.data,
+      error => this.getThreeProducterrorAction(error)
+      );
+  }
+
+  public getThreeProductDoneAction(data) {
+    console.log("data = ", data);
+    this.slideHead = data.data;
+    if (this.slideHead.length > 0) {
+      for (let z = 0; z < this.slideHead.length; z++) {
+        this.slideHead[z].product_path = this.imgLink + this.slideHead[z].product_path;
+      }
+    }
+    console.log(this.slideHead);
+  }
+
+  public getThreeProducterrorAction(error) {
+    console.log("error = ", error);
   }
 
 }
